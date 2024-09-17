ccfdc0a19b2c572b0fe5427ea758e30edc250274
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-09-17 20:58:28 +0530

    fix: celery task

--- web/AMS/tasks.py
+++ web/AMS/tasks.py
@@ -4,6 +4,7 @@ from AMS.utils import fetch_att
 from datetime import datetime
 from celery.utils.log import get_task_logger
 import requests
+import json
 
 logger = get_task_logger(__name__)
 
@@ -19,16 +20,28 @@ def fetch_attendances(name="AMS.fetch_attendances"):
         password = student.get_password()  # Use the method to decrypt the password
 
         try:
-            student_name, percentage, end_date = fetch_att(username, password)
+            student_name, percentage, end_date, subs, subs_percents = fetch_att(
+                username, password
+            )
             # Convert end_date string to a date object
             end_date = datetime.strptime(end_date, "%d-%m-%Y").date()
 
+            subject_details = {}
+            for sub, per in zip(subs, subs_percents):
+                subject_details[sub] = float(per[:-2])
+
+            logger.info(json.dumps(subject_details))
+
             # Save the attendance data
             attendance = Attendance(
-                student=student, date=end_date, percentage=float(percentage.strip("%"))
+                student=student,
+                date=end_date,
+                percentage=float(percentage.strip("%")),
+                subject_details=subject_details,
             )
-            logger.info(f"Success for {student_name}")
             attendance.save()
+
+            logger.info(f"Successfully updated attendance for {student_name}")
 
             requests.post(
                 f"https://ntfy.foss.rizexor.com/{student.ntfy_topic}",
